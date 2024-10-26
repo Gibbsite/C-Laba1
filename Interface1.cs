0f6de7913984ade527b6b6558b8b80f007040e17
Gibbsite <113031236+Gibbsite@users.noreply.github.com>
2024-10-26 21:16:47 +0300

    Add files via upload

---
+++ Interface1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackLibrary
+{
+    public interface IStack
+    {
+        void Push(string value);
+        string Read();
+        string Pop();
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using StackLibrary;
+
+namespace StackTests
+{
+    [TestClass]
+    public class StackTest
+    {
+        [TestMethod]
+        public void PushItem_Read_ReturnString()
+        {
+            IStack stack = new Stack();
+            const string group = "ІТШІ-23-1";
+
+            stack.Push(group);
+
+            Assert.AreEqual(group, stack.Read(), "WRONG");
+        }
+
+        [TestMethod]
+        public void PushItem_Pop_ReturnString()
+        {
+            IStack stack = new Stack();
+            const string group = "ІТШІ-23-1";
+
+            stack.Push(group);
+
+            Assert.AreEqual(group, stack.Pop(), "WRONG");
+        }
+
+        [TestMethod]
+        public void PushItem_Pop_ReturnString_PopReturnStringError()
+        {
+            IStack stack = new Stack();
+            const string group = "ІТШІ-23-1";
+
+            stack.Push(group);
+
+            Assert.AreEqual(group, stack.Pop(), "WRONG");
+            Assert.AreEqual("Stack is empty", stack.Pop(), "WRONG");
+        }
+    }
+}
